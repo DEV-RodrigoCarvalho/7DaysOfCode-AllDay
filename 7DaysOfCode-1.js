0aacb45e9a7efa6ed04eaf95727264c6fda9d932
RodrigoSilva-DEV <rodrigodigo681@gmail.com>
2025-09-04 22:01:46 -0400

    SecundDay

---
+++ 7DaysOfCode-1.js
@@ -0,0 +1,29 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+// Verificar se o valor é igual, mas os tipos são diferentes.
+if (numeroUm == stringUm && typeof numeroUm != typeof stringUm) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+// Verificar se o valor E o tipo são iguais. os tipos são diferentes o resultado e falto
+if (numeroTrinta === stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+//Verifica o valor é igual, mas são de tipos diferentes.
+if (numeroDez == stringDez && typeof numeroDez != typeof stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
+
+{};
